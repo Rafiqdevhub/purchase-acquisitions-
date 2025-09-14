e9898088fc89de90048f50c6001293c40222e90b
rakiqdevhub <rafiq85825@gmail.com>
2025-09-14 15:41:27 +0500

    Add Middlewares

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 import express from 'express';
-import logger from './config/logger.js';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
 const app = express();
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
 
 app.get('/', (req, res) => {
   logger.info('Hello from Purchase!...');
