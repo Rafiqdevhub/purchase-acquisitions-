12c0f04b6163cb7cd1dcf06a3099b0f73376a6e4
rakiqdevhub <rafiq85825@gmail.com>
2025-09-14 15:27:21 +0500

    Logger added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import logger from './config/logger.js';
 const app = express();
 
 app.get('/', (req, res) => {
+  logger.info('Hello from Purchase!...');
   res.status(200).send('Hello from Purchase!...');
 });
 
