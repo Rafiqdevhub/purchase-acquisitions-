8cda74b11beddafb85402dc5683bffa35bf9ae84
rakiqdevhub <rafiq85825@gmail.com>
2025-09-14 14:42:11 +0500

    models added.

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from Purchase!');
+  res.status(200).send('Hello from Purchase!...');
 });
 
 app.get('/health', (req, res) => {
